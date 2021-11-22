a69d1e605a9a90e2e66c204cc17e49d367c38d81
erlan <kalybekov.erlan@gmail.com>
2021-11-23 01:46:01 +0600

    Controller class implements KeyListener

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,7 @@
-public class Controller {
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class Controller implements KeyListener {
     private Model model;
 
     public Controller(Viewer viewer) {
@@ -6,8 +9,25 @@ public class Controller {
         model = new Model(viewer);
 
     }
+
     public Model getModel() {
         return model;
     }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        System.out.println(keyCode);
+
+    }
+
+
 
 }
