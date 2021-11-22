242e350fa24517fb31724e980364f51bc7886d57
erlan <kalybekov.erlan@gmail.com>
2021-11-23 02:14:11 +0600

    JFrame added

--- src/Viewer.java
+++ src/Viewer.java
@@ -1,10 +1,19 @@
+import javax.swing.JFrame;
+import java.awt.event.KeyListener;
+
 public class Viewer {
 
     public Viewer() {
-        System.out.println("Viewer: "+this);
+        System.out.println("Viewer: " + this);
         Controller controller = new Controller(this);
         Model model = controller.getModel();
         Canvas canvas = new Canvas(model);
+
+        JFrame frame = new JFrame("Sokoban Game MVC Pattern");
+        frame.setSize(600, 600);
+        frame.setLocation(300, 300);
+        frame.setVisible(true);
+        frame.addKeyListener(controller);
     }
 
 }
