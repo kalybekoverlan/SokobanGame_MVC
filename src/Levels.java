84c986d5c200e354cd7fecc5a34a6c2aa73b1001
erlan kalybekov <kalybekov.erlan@gmail.com>
2021-11-30 10:17:13 +0600

    Defined class Levels

---
+++ src/Levels.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Levels {
+    private int level;
+
+    public Levels() {
+            level=4;
+    }
+
+    public int[][] nextLevel(File file) {
+        desktop[][] = null;
+        return desktop;
+    }
+
+    public String getContentFile() {
+
+        return null;
+    }
+
+
+}
