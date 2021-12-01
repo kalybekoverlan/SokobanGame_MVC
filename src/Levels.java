65a3ff1e1a41ca3bc7e3c644e1c5242b4c0497a8
erlan kalybekov <kalybekov.erlan@gmail.com>
2021-12-01 14:06:54 +0600

    Refactored getContentFile method

--- src/Levels.java
+++ src/Levels.java
@@ -13,28 +13,41 @@ public class Levels {
     public int[][] nextLevel(String fileName) {
         int[][] desktop = null;
         File file = new File(fileName);
-        getContentFile(file);
+        try{
+            String contentFile = getContentFile(file);
+            System.out.println(contentFile);
+        } catch (Exception e) {
+            System.out.println("Error : " + e);
+        }
+
         return desktop;
     }
 
-    private String getContentFile(File file) {
+    private String getContentFile(File file) throws Exception{
 
         try(FileInputStream in = new FileInputStream(file)) {
-
+            int size = (int) file.length();
+            char[] array = new char[size];
+            int index = 0;
             int unicode;
+            //System.out.println("file size: "+size);
             while((unicode=in.read()) != -1) {
                 char symbol = (char) unicode;
-                System.out.print(symbol);
+                if(('0' <= symbol && symbol <= '4') || (symbol == '\n')) {
+                    array[index] = symbol;
+                    index = index + 1;
+                }
             }
-            System.out.print("will arrive after parsing");
 
-        } catch (FileNotFoundException e) {
-            return null;
-        } catch (IOException e) {
-
+            array[index++] = '\n';
+            String content = new String(array, 0, index);
+            return content;
+        } catch (FileNotFoundException fnfe) {
+            throw new Exception("File Not Found Exception : " + fnfe);
+        } catch (IOException ioe) {
+            throw new Exception("Basic I/O Exception : " + ioe);
         }
 
-        return null;
     }
 
 
