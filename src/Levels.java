9296faa6daf4ac9a8a182ee934ced51a93e03352
erlan kalybekov <kalybekov.erlan@gmail.com>
2021-12-01 23:08:44 +0600

    The following method convert String line then create a two-dimensional array in which
    the sizes of the second dimension are may be unequal.

--- src/Levels.java
+++ src/Levels.java
@@ -25,6 +25,53 @@ public class Levels {
 
     private int[][] convertStringIntoTwoDimensionArray(String line) {
         System.out.println(line);
+        int n = line.length();
+        int row = 0;
+        for(int i = 0; i < n; i++) {
+            char symbol = line.charAt(i);
+            if(symbol == '\n') {
+                row = row + 1;
+            }
+        }
+
+        int[][] array = new int[row][];
+        int column = 0;
+        int a = 0;
+
+        for(int i = 0; i < n; i++) {
+            char symbol = line.charAt(i);
+            if(symbol != '\n') {
+                column = column + 1;
+            }
+            if(symbol == '\n') {
+                array[a] = new int[column];
+                a = a + 1;
+                column = 0;
+            }
+        }
+
+        row = 0;
+        column = 0;
+        for(int i = 0; i < line.length(); i++) {
+            char symbol = line.charAt(i);
+            if(symbol != '\n') {
+                array[row][column] = Integer.parseInt("" + symbol);
+                column = column + 1;
+            }
+            if(symbol == '\n') {
+                row = row + 1;
+                column = 0;
+            }
+        }
+
+        System.out.println("after converting to two-dimensional array");
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+
         return null;
     }
 
@@ -43,8 +90,7 @@ public class Levels {
                     index = index + 1;
                 }
             }
-
-            array[index++] = '\n';
+            //array[index++] = '\n';
             String content = new String(array, 0, index);
             return content;
         } catch (FileNotFoundException fnfe) {
