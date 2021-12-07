75ce01d8cff053427693180b8cefff4de6bc86be
erlan kalybekov <kalybekov.erlan@gmail.com>
2021-12-07 13:12:02 +0600

    Some refactoring and additions

--- SokobanLevelServer/Client.java
+++ SokobanLevelServer/Client.java
@@ -1,14 +1,19 @@
 import java.net.Socket;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Client {
     public static void main(String args[]) {
         // hostname of home virtual server
         String hostName = "192.168.204.129";
         int portNumber = 4446;
-        try {
-            System.out.println("Connection to : Sokoban Level Server");
-            Socket socket = new Socket(hostName, portNumber);
+        System.out.println("Connection to : Sokoban Level Server");
+
+        try(Socket socket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
+            ) {
+            String message = "Send me level";
+            out.println(message);
             System.out.println("Close connection");
         } catch (IOException ioe) {
             System.out.println("Sokoban Level Server Error : " + ioe);
--- SokobanLevelServer/SokobanLevelServer.java
+++ SokobanLevelServer/SokobanLevelServer.java
@@ -1,6 +1,8 @@
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class SokobanLevelServer {
     public static void main(String args[]) {
@@ -9,7 +11,12 @@ public class SokobanLevelServer {
         try {
             ServerSocket serverSocket = new ServerSocket(portNumber);
             Socket clientSocket = serverSocket.accept();
+            InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader in = new BufferedReader(inputStreamReader);
+            String infoFromClient = in.readLine();
             System.out.println("Client socket : " + clientSocket);
+            System.out.println("From client : " + infoFromClient);
+            String desktop = "2222222222";
         } catch (IOException ioe) {
             System.out.println("Sokoban Level Server Error : " + ioe);
         }
