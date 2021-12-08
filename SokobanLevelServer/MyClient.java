e62634b46579187e4e19c0e04f17dfc3b1eded27
erlan kalybekov <kalybekov.erlan@gmail.com>
2021-12-08 13:51:37 +0600

    Refactoring MyClient which extends Thread

--- SokobanLevelServer/MyClient.java
+++ SokobanLevelServer/MyClient.java
@@ -1,3 +1,15 @@
-public class MyClient {
-    
+import java.net.Socket;
+
+public class MyClient extends Thread {
+    private Socket socket;
+
+    public MyClient(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        System.out.println("socket : " + socket);
+
+        socket.close();
+    }
 }
