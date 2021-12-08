1b3f866696d6fb9605778c5e94b26fecd203bf40
erlan kalybekov <kalybekov.erlan@gmail.com>
2021-12-08 12:43:33 +0600

    Instance variable serverSocket and constructor for that

--- SokobanLevelServer/SokobanLevelServer.java
+++ SokobanLevelServer/SokobanLevelServer.java
@@ -1,32 +1,43 @@
-import java.net.Socket;
+// import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// import java.io.OutputStreamWriter;
+// import java.io.PrintWriter;
 
 public class SokobanLevelServer {
-    public static void main(String args[]) {
-        System.out.println("Sokoban Level Server started");
-        int portNumber = 4446;
+
+    private ServerSocket serverSocket;
+
+    public SokobanLevelServer(int portNumber) {
+
         try {
-            ServerSocket serverSocket = new ServerSocket(portNumber);
-            Socket clientSocket = serverSocket.accept();
-            InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader in = new BufferedReader(inputStreamReader);
-            String infoFromClient = in.readLine();
-            System.out.println("Client socket : " + clientSocket);
-            System.out.println("From client : " + infoFromClient);
-
-            String desktop = "2222222222";
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            out.println(desktop);
-            System.out.println("level : " + desktop + " sended");
-
+            serverSocket = new ServerSocket(portNumber);
         } catch (IOException ioe) {
-            System.out.println("Sokoban Level Server Error : " + ioe);
         }
+
     }
+    // public static void main(String args[]) {
+    //     System.out.println("Sokoban Level Server started");
+    //     int portNumber = 4446;
+    //     try {
+    //         ServerSocket serverSocket = new ServerSocket(portNumber);
+    //         Socket clientSocket = serverSocket.accept();
+    //         InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+    //         BufferedReader in = new BufferedReader(inputStreamReader);
+    //         String infoFromClient = in.readLine();
+    //         System.out.println("Client socket : " + clientSocket);
+    //         System.out.println("From client : " + infoFromClient);
+    //
+    //         String desktop = "2222222222";
+    //         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+    //         out.println(desktop);
+    //         System.out.println("level : " + desktop + " sended");
+    //
+    //     } catch (IOException ioe) {
+    //         System.out.println("Sokoban Level Server Error : " + ioe);
+    //     }
+    // }
 
 }
