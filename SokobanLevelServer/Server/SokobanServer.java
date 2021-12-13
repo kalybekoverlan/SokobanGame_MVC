7155982afef19cfd77c047b7445761a91ebeb126
erlan <kalybekov.erlan@gmail.com>
2021-12-13 13:20:55 +0600

    Rename SokobanLevelServer to SokobanServer

--- SokobanLevelServer/Server/SokobanServer.java
+++ SokobanLevelServer/Server/SokobanServer.java
@@ -1,17 +1,13 @@
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
-// import java.io.BufferedReader;
-// import java.io.InputStreamReader;
-// import java.io.OutputStreamWriter;
-// import java.io.PrintWriter;
 
-public class SokobanLevelServer {
+public class SokobanServer {
 
     private ServerSocket serverSocket;
     private static final Object object = new Object();
 
-    public SokobanLevelServer(int portNumber) {
+    public SokobanServer(int portNumber) {
 
         try {
             serverSocket = new ServerSocket(portNumber);
